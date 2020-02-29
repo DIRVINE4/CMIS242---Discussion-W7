eaade1f50ac63fb126a59e103956e97f362059a0
donaldirvine <irvine.dj@icloud.com>
2020-02-29 08:51:20 -0700

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("FIRST LINE OF DISCUSSION 7 FOR GIT!!!!!");
+
+	//test test test
+    }
+}
