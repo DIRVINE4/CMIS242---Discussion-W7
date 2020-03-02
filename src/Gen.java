95a60510c2e1d691d59f78297a47cd439dd315d5
donaldirvine <irvine.dj@icloud.com>
2020-03-01 19:55:33 -0500

    Doing a non-generic vs. generic array list usage to show the benefits.

---
+++ src/Gen.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Gen
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> arrayList = new ArrayList<>();
+
+    }
+}
+
---
+++ src/com/company/NonGen.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class NonGen {
+
+    public static void main(String[] args)
+    {
+        ArrayList arrayList = new ArrayList();
+
+
+    }
+}
