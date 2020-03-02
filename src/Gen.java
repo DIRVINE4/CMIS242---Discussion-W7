c34592d4663430ff38dd0fa783e989d98a0c33c1
donaldirvine <irvine.dj@icloud.com>
2020-03-01 20:48:08 -0500

    Non generic class complete

--- src/Gen.java
+++ src/Gen.java
@@ -1,12 +1,27 @@
-/* import java.util.ArrayList;
+package com.company;
 
-public class Gen
-{
+import java.util.ArrayList;
+
+public class Gen {
+
     public static void main(String[] args)
     {
-        ArrayList<String> arrayList = new ArrayList<>();
+        String word1, word2, word3, word4, word5;
 
+        ArrayList<String> arrayList = new ArrayList();
+
+        arrayList.add(249.22); //The generic class saves time by catching errors!
+        arrayList.add("Work?");
+        arrayList.add(0.284373839282983198238712398723); //The generic class saves time by catching errors!
+        arrayList.add("Does");
+        arrayList.add("This");
+
+        word1 = (String) arrayList.get(3);
+        word2 = (String) arrayList.get(4);
+        word3 = (String) arrayList.get(1);
+        word4 = (String) arrayList.get(0);
+        word5 = (String) arrayList.get(2);
+
+        System.out.println(word1 + word2 + word3 + word4 + word5);
     }
 }
-
-*/
